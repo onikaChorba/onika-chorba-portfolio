6f517d50dcda740bb1cc28270d4d191f7460abe7
Onika Chorba <110539259+onikaChorba@users.noreply.github.com>
2025-10-06 16:23:35 +0300

    feat: footer

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,14 @@
 import { Header } from "./header";
 import Form from "./form/Form.vue";
 import Modal from "./modal/Modal.vue";
+import Footer from "./footer/Footer.vue";
 import Canvas from "./canvas/Canvas.vue";
 import Project from "./project/Project.vue";
 
 export {
   Form,
   Modal,
+  Footer,
   Header,
   Canvas,
   Project
