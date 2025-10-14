260c05d937c0e1ea2317f4d9aef40f0532c676c2
Onika Chorba <110539259+onikaChorba@users.noreply.github.com>
2025-10-14 11:25:35 +0300

    fix: header links, hero, contact, projects locale

--- src/sections/contact/Contact.vue
+++ src/sections/contact/Contact.vue
@@ -1,13 +1,6 @@
-<script setup lang="ts">
-import { Form } from '@/components';
-import { useI18n } from 'vue-i18n';
-
-const { t } = useI18n();
-</script>
-
 <template>
   <section class="contact" id="contact">
-    <h2 class="contact__title title2">{{ t('contact.contact') }}</h2>
+    <h2 class="contact__title title2">{{ contactTranslations.contact t('contact.contact') }}</h2>
     <p class="text">
       <i18n-t keypath="contact.contactText">
         <template #resumeLink>
@@ -19,10 +12,44 @@ const { t } = useI18n();
       </i18n-t>
     </p>
     <div class="section">
-      <Form />
+      <Form :contact-translations="contactTranslations" />
     </div>
   </section>
 </template>
+
+<script setup lang="ts">
+import { watch } from 'vue';
+import { useI18n } from 'vue-i18n';
+import { Form } from '@/components';
+import { ref, onMounted } from 'vue';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../firebase/firebase.config';
+
+const { locale, t, setLocaleMessage } = useI18n<{ locale: string; t: any }>();
+const contactTranslations = ref<Record<string, string>>({});
+
+onMounted(async () => {
+  const contactDoc = doc(db, "locales", locale.value);
+  const contactSnap = await getDoc(contactDoc);
+  if (contactSnap.exists()) {
+    const messages = contactSnap.data();
+    contactTranslations.value = messages.contact || {};
+    setLocaleMessage(locale.value, messages);
+  } else {
+    console.warn("❌ Немає перекладів для about у Firestore");
+  }
+});
+
+watch(locale, async (newLocale) => {
+  const contactDoc = doc(db, "locales", newLocale);
+  const contactSnap = await getDoc(contactDoc);
+  if (contactSnap.exists()) {
+    const messages = contactSnap.data();
+    contactTranslations.value = messages.about || {};
+    setLocaleMessage(newLocale, messages);
+  }
+});
+</script>
 
 <style scoped lang="scss">
 .link {
