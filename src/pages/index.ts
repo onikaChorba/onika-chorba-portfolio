87e22d8a74817133d993cdba25b4b038fef0b7b7
Onika Chorba <110539259+onikaChorba@users.noreply.github.com>
2025-10-24 11:18:42 +0300

    feat: resume client

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
 import { FrontPage } from "./front-page";
+import ResumeClient from "./resume/ResumeClient.vue";
 import { AdminPageProjects, AdminPageMain, Resume } from './admin';
 
-export { FrontPage, AdminPageProjects, AdminPageMain, Resume }
+export { FrontPage, AdminPageProjects, AdminPageMain, Resume, ResumeClient }
---
+++ src/pages/resume/index.ts
@@ -0,0 +1,3 @@
+import ResumeClient from "./ResumeClient.vue";
+
+export { ResumeClient }
